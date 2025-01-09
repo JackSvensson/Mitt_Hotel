d0cad67c276e7141a36d830fbb2980927c8fde3a
JackSvensson <Jackens1997@gmail.com>
2025-01-09 18:00:10 +0100

    Update db_config.php

--- Config/db_config.php
+++ Config/db_config.php
@@ -2,16 +2,12 @@
 
 declare(strict_types=1)
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');     // Replace with your MySQL username
-define('DB_PASSWORD', '');         // Replace with your MySQL password
-define('DB_NAME', 'glass_onion_hotel');
+define('DB_PATH', '/Users/jacksvensson/Desktop/WebDev/Hotel/Booking Hotel.sqllite3'); // Update this with your full path
 
-// Create database connection
 try {
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    // Set the PDO error mode to exception
+    $pdo = new PDO("sqlite:" . DB_PATH);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('PRAGMA foreign_keys = ON;');
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
